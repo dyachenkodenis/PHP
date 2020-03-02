1086924e06cdfabf5454e45e75e460b73d4fd3cc
dyachenkodenis <denisdyachenko.90@gmail.com>
2020-03-02 06:05:31 +0500

    add basic.php

---
+++ basic_syntax/basic.php
@@ -0,0 +1,31 @@
+<?php
+# ... code
+
+echo "Hello World";
+// ... more code
+
+echo "<br />";
+// ... html
+
+echo "Last statement"; /*  ... more code
+	...*/
+
+
+?>
+
+<p><br /></p>
+
+<?php 
+	   		if($espresso = "espresso"):?>
+
+espresso
+
+<?php 		else:
+					?>
+
+cappuccino
+
+<?php 		endif; 
+					?>
+
+
