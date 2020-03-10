6a46d0c03f5ca7f05eab8359118ed098ad565b42
dyachenkodenis <denisdyachenko.90@gmail.com>
2020-03-10 06:12:10 +0500

    edit file objects.php

---
+++ basic_syntax/objects.php
@@ -0,0 +1,25 @@
+<?php
+
+$html = "<p><br /></p>";// html/
+
+class coffe{
+	function my_coffe(){
+		echo "Cappuccino";
+	}
+}
+
+$coffe = new coffe;
+$coffe->my_coffe();
+
+////////////////////
+echo $html; // html/
+////////////////////
+
+
+$obj = (object) 'Test';
+var_dump($obj->scalar);
+
+
+////////////////////
+echo $html; // html/
+////////////////////
