e04ca92e86bb6707108f6b4bf130f7f81db64766
dyachenkodenis <denisdyachenko.90@gmail.com>
2020-07-07 17:23:36 +0500

    added file posix_extended_regexp.php

---
+++ regular_expressions/posix_extended_regexp.php
@@ -0,0 +1,25 @@
+<?php
+
+/*ereg(), eregi(), ereg_replace(), eregi_replace(), split(), spliti(), sql_regcase() removed in php7
+
+*/
+
+//preg_match()
+preg_match('/(foo)(bar)(baz)/', 'foobarbaz', $test, PREG_OFFSET_CAPTURE);
+print_r($test);
+echo "<br />";
+
+//explode()
+$mycoffe = "Capuccino FlatWhite Espresso";
+$coffe = explode(" ", $mycoffe);
+echo $coffe[0]."<br />";
+echo $coffe[1]."<br />";
+
+//preg_split()
+$test1 = preg_split("/[\s,]+/", "hupertext language,programming");
+print_r($test1);
+
+//preg_quote()
+$test2 = '$40 for a g3/400';
+$test2 = preg_quote($test2, '/');
+echo $test2 . "<br />";  // returns \$40 for a g3\/400
